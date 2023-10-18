93c0a3587f9f4d28a24b0fcdf7ada1595a6118a0
Pavan Kumar Kandapagari <pavan.kandapagari@agile-robots.com>
2023-10-18 11:36:07 +0200

    feat(models): added Transformer class in transformer.py
    The Transformer class, a new addition to the models module, inherits from the nn.Module class. It includes an initialization method for setting up transformer layers (TransformerAttention and FeedForward layers) and a forward method for applying these layers to input data.
    
    This class is essential for the transformer model's operation in the robotic_transformer project, allowing the creation of transformer models with customizable dimensions, head counts, depths, and dropout rates.

---
+++ robotic_transformer/models/transformer.py
@@ -0,0 +1,39 @@
+from torch import nn
+from beartype import beartype
+from models.attention import TransformerAttention
+from models import FeedForward
+from typing import Optional, Tuple, Callable
+from utils import default
+
+
+@beartype
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        dim,
+        dim_head=64,
+        heads=8,
+        depth=6,
+        attn_dropout=0.,
+        ff_dropout=0.
+    ):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                TransformerAttention(dim=dim, heads=heads,
+                                     dropout=attn_dropout),
+                FeedForward(dim=dim, dropout=ff_dropout)
+            ]))
+
+    def forward(
+        self,
+        x,
+        cond_fns: Optional[Tuple[Callable, ...]] = None,
+        attn_mask=None
+    ):
+        cond_fns = iter(default(cond_fns, []))
+        for attn, ff in self.layers:
+            x = attn(x, attn_mask=attn_mask, cond_fn=next(cond_fns, None)) + x
+            x = ff(x, cond_fn=next(cond_fns, None)) + x
+        return x
