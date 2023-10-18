669dcc1222d288af4ed7f455f030f6dfb9a25b59
Pavan Kumar Kandapagari <pavan.kandapagari@agile-robots.com>
2023-10-18 11:30:47 +0200

    feat(layer_norm): added LayerNorm class in robotic_transformer/models
    A new LayerNorm class, inheriting from PyTorch's nn.Module class, has been added to the robotic_transformer/models directory. This class includes an initialization method for setting up the gamma parameter and registering the beta buffer. The forward method applies layer normalization to the input tensor using these parameters.

---
+++ robotic_transformer/models/layer_norm.py
@@ -0,0 +1,13 @@
+from torch import nn
+import torch
+
+import torch.nn.functional as F
+
+class LayerNorm(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.ones(dim))
+        self.register_buffer("beta", torch.zeros(dim))
+
+    def forward(self, x):
+        return F.layer_norm(x, x.shape[-1:], self.gamma, self.beta)
